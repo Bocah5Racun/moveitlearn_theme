776e8e4e3921dcb9eb7757b71f6735262f57b34e
Bocah5Racun <onlymoderatelymad@gmail.com>
2025-05-31 23:22:05 +0800

    Fixed progress never reaching 100%.

--- includes/scripts/progress-bar.js
+++ includes/scripts/progress-bar.js
@@ -1,10 +1,12 @@
 const progressBar = document.getElementById("singular-progress-bar");
 const mainDiv = document.getElementById("singular-main-wrapper");
+const footer = document.getElementById("footer");
 
 window.addEventListener("scroll", () => {
   const scrollPos = window.scrollY || window.pageYOffset;
+  const footerHeight = footer.offsetHeight;
   const mainTop = mainDiv.offsetTop;
-  const mainHeight = mainDiv.offsetHeight;
+  const mainHeight = mainDiv.offsetHeight - footerHeight;
 
   const distanceScrolledInMain = scrollPos - mainTop;
   const scrollPercent =
