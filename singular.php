358d2a391dfc56ab0078c69baea17f93fb4b4b88
Bocah5Racun <onlymoderatelymad@gmail.com>
2025-05-31 21:48:33 +0800

    Added featured image to singular.

--- singular.php
+++ singular.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 
+<img src="<?= get_the_post_thumbnail_url(); ?>" alt="" class="singular-featured-img">
 <main id="singular-main-wrapper" class="container container--page text-container">
     <h1 class="singular-title"><?= get_the_title(); ?></h1>
     <?php the_content(); ?>
