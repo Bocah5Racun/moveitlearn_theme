831d13a0e62e6bcd65f6731fe9d91f777c1f5465
Bocah5Racun <onlymoderatelymad@gmail.com>
2025-05-31 21:53:05 +0800

    Tweaked singular styles.

--- singular.php
+++ singular.php
@@ -1,8 +1,9 @@
 <?php get_header(); ?>
 
 <img src="<?= get_the_post_thumbnail_url(); ?>" alt="" class="singular-featured-img">
+<h1 class="singular-title"><?= get_the_title(); ?></h1>
+
 <main id="singular-main-wrapper" class="container container--page text-container">
-    <h1 class="singular-title"><?= get_the_title(); ?></h1>
     <?php the_content(); ?>
 </main>
 
