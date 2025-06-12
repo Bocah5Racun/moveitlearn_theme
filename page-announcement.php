c0ecfb57ae41e4fba282992f3c53c354eaa55aab
Andi Nuruljihad <andi.nuruljihad@gmail.com>
2025-06-12 23:23:00 +0800

    Tweaked inline styles.

--- page-announcement.php
+++ page-announcement.php
@@ -5,14 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
     <style>
+
+        body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+        }
+
         #announcement-container {
             width: clamp(380px, 100%, 65ch);
         }
+        
     </style>
 </head>
 <body>
 
     <main id="announcement-container">
+        <h1><?php the_title(); ?></h1>
         <?php the_content(); ?>
     </main>
     
