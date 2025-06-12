bb5e88a887635f10dec7c52c503445f8b8a8b586
Andi Nuruljihad <andi.nuruljihad@gmail.com>
2025-06-12 23:34:41 +0800

    Styling for contents.

--- page-announcement.php
+++ page-announcement.php
@@ -13,10 +13,19 @@
         }
 
         #announcement-container {
+            align-items: center;
+            display: flex;
+            flex-direction: column;
             font-size: 18px;
-            width: clamp(380px, 100%, 65ch);
+            gap: 1rem;
             margin: 0 auto;
             padding: 1rem;
+            width: clamp(380px, 100%, 65ch);
+        }
+
+        #announcement-container > img {
+            width: 180px;
+            height: auto;
         }
 
     </style>
@@ -26,12 +35,14 @@
     <main id="announcement-container">
         <?php the_custom_logo(); ?>
         <h1>Move It Learn Ditangguhkan</h1>
-        <p>Kami dari Tim Move It dengan sedih mengumumkan bahwa Move It Learn ditangguhkan <b>atas keputusan Dewan Direksi AIDRAN untuk jangka waktu yang belum ditentukan.</b></p>
-        <p>Kami mengucapkan terima kasih kepada semua pihak yang telah berkontribusi dalam pengembangan Move It Learn dan Proyek Move It. Permohonan maaf yang sebesar-besarnya kami sampaikan kepada siapa pun yang telah memberikan masukan demi menjadikan Move It Learn sebagai sarana yang lebih baik untuk pendidikan inklusi di Indonesia, serta kepada siapa pun yang saat ini sedang mengikuti salah satu kursus di platform ini.</p>
-        <p>Hingga saat ini kami belum dapat memastikan kapan Move It Learn akan diaktifkan kembali.</p>
-
-        <p>Terima kasih,</p>
-        <p><b>Tim Move It</b></p>
+        <div>
+            <p>Kami dari Tim Move It dengan sedih mengumumkan bahwa Move It Learn ditangguhkan <b>atas keputusan Dewan Direksi AIDRAN untuk jangka waktu yang belum ditentukan.</b></p>
+            <p>Kami mengucapkan terima kasih kepada semua pihak yang telah berkontribusi dalam pengembangan Move It Learn dan Proyek Move It. Permohonan maaf yang sebesar-besarnya kami sampaikan kepada siapa pun yang telah memberikan masukan demi menjadikan Move It Learn sebagai sarana yang lebih baik untuk pendidikan inklusi di Indonesia, serta kepada siapa pun yang saat ini sedang mengikuti salah satu kursus di platform ini.</p>
+            <p>Hingga saat ini kami belum dapat memastikan kapan Move It Learn akan diaktifkan kembali.</p>
+            
+            <p>Terima kasih,</p>
+            <p><b>Tim Move It</b></p>
+        </div>
     </main>
     
     <?php wp_footer(); ?>
