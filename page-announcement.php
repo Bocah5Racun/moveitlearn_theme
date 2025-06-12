2a0dbe676005cedc243e933f027a163bf9f42739
Andi Nuruljihad <andi.nuruljihad@gmail.com>
2025-06-12 23:29:52 +0800

    Hardcoded the message instead of relying on dynamically loading it.

--- page-announcement.php
+++ page-announcement.php
@@ -13,16 +13,24 @@
         }
 
         #announcement-container {
+            font-size: 18px;
             width: clamp(380px, 100%, 65ch);
+            margin: 0 auto;
+            padding: 1rem;
         }
-        
+
     </style>
 </head>
 <body>
 
     <main id="announcement-container">
-        <h1><?php the_title(); ?></h1>
-        <?php the_content(); ?>
+        <h1>Move It Learn Ditangguhkan</h1>
+        <p>Kami dari Tim Move It dengan sedih mengumumkan bahwa Move It Learn ditangguhkan <b>atas keputusan Dewan Direksi AIDRAN untuk jangka waktu yang belum ditentukan.</b></p>
+        <p>Kami mengucapkan terima kasih kepada semua pihak yang telah berkontribusi dalam pengembangan Move It Learn dan Proyek Move It. Permohonan maaf yang sebesar-besarnya kami sampaikan kepada siapa pun yang telah memberikan masukan demi menjadikan Move It Learn sebagai sarana yang lebih baik untuk pendidikan inklusi di Indonesia, serta kepada siapa pun yang saat ini sedang mengikuti salah satu kursus di platform ini.</p>
+        <p>Hingga saat ini kami belum dapat memastikan kapan Move It Learn akan diaktifkan kembali.</p>
+
+        <p>Terima kasih,</p>
+        <p><b>Tim Move It</b></p>
     </main>
     
     <?php wp_footer(); ?>
